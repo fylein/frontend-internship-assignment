e376dd698a2f18e8a4a940fa69a6691b73e6206d
Karan Ravi <Karanravi2015@gmail.com>
2023-05-18 02:41:12 +0530

    new features added

--- src/app/search-results-table/search-results-table.component.ts
+++ src/app/search-results-table/search-results-table.component.ts
@@ -1,15 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, finalize } from 'rxjs/operators';
+import { of } from 'rxjs';
 
-interface Book {
+export interface Book {
   title: string;
   author_name: string[];
   first_publish_year: number;
   // Add more properties as needed
-}
-
-interface SearchResponse {
-  docs: any[]; // Replace 'any' with a more specific interface if available
 }
 
 @Component({
@@ -19,26 +17,33 @@ interface SearchResponse {
 })
 export class SearchResultsTableComponent {
   @Input() searchResults: Book[] = [];
-  private apiUrl = 'https://openlibrary.org/search.json';
+  isLoading = false;
 
   constructor(private http: HttpClient) {}
 
   searchBooks(searchTerm: string): void {
-    const url = `${this.apiUrl}?q=${searchTerm}`;
+    const url = `https://openlibrary.org/search.json?q=${searchTerm}`;
 
-    this.http.get<SearchResponse>(url).subscribe(
-      (response: SearchResponse) => {
+    this.isLoading = true; // Set loading state to true before making the request
+
+    this.http.get(url)
+      .pipe(
+        catchError((error) => {
+          console.error('Error occurred while searching books:', error);
+          // Handle the error and return an empty array or show an error message
+          return of([]);
+        }),
+        finalize(() => {
+          this.isLoading = false; // Set loading state to false after the request completes (success or error)
+        })
+      )
+      .subscribe((response: any) => {
         // Extract the necessary information from the API response and format it into the Book interface
         this.searchResults = response.docs.map((book: any) => ({
           title: book.title,
           author_name: book.author_name || ['Unknown Author'],
           first_publish_year: book.first_publish_year || 0,
         }));
-      },
-      (error: any) => {
-        console.error('Error fetching search results:', error);
-        // Handle the error appropriately, e.g., display an error message
-      }
-    );
+      });
   }
 }
