d28bf6a95228b9dc94b85d9b1ac9ea7e9eba0563
Karan Ravi <Karanravi2015@gmail.com>
2023-05-18 02:35:25 +0530

    new features added

--- src/app/components/trending-subjects/trending-subjects.component.ts
+++ src/app/components/trending-subjects/trending-subjects.component.ts
@@ -9,9 +9,10 @@ import { Book } from 'src/app/core/models/book-response.model';
   styleUrls: ['./trending-subjects.component.scss'],
 })
 export class TrendingSubjectsComponent implements OnInit {
-  isLoading: boolean = true;
-  subjectName: string = '';
+  isLoading = true;
+  subjectName = '';
   allBooks: Book[] = [];
+  errorMessage = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -19,27 +20,29 @@ export class TrendingSubjectsComponent implements OnInit {
     private subjectsService: SubjectsService
   ) {}
 
-  getAllBooks() {
-    this.isLoading = true; // Set loading state to true before fetching data
-
-    this.subjectsService.getAllBooks(this.subjectName).subscribe(
-      (data) => {
-        this.allBooks = data?.works;
-      },
-      (error) => {
-        console.log('Error occurred while fetching books:', error);
-      },
-      () => {
-        this.isLoading = false; // Set loading state to false after data is fetched
-      }
-    );
-  }
-
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.subjectName = params.get('name') || '';
       this.getAllBooks();
     });
+  }
+
+  getAllBooks(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.subjectsService.getAllBooks(this.subjectName).subscribe(
+      (data: any) => {
+        this.allBooks = data?.works || [];
+      },
+      (error: any) => {
+        console.error('Error occurred while fetching books:', error);
+        this.errorMessage = 'Failed to fetch books. Please try again later.';
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
   }
 
   goBack(): void {
--- src/app/search-results-table/search-results-table.component.ts
+++ src/app/search-results-table/search-results-table.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export interface Book {
+interface Book {
   title: string;
   author_name: string[];
   first_publish_year: number;
   // Add more properties as needed
+}
+
+interface SearchResponse {
+  docs: any[]; // Replace 'any' with a more specific interface if available
 }
 
 @Component({
@@ -15,19 +19,26 @@ export interface Book {
 })
 export class SearchResultsTableComponent {
   @Input() searchResults: Book[] = [];
+  private apiUrl = 'https://openlibrary.org/search.json';
 
   constructor(private http: HttpClient) {}
 
   searchBooks(searchTerm: string): void {
-    const url = `https://openlibrary.org/search.json?q=${searchTerm}`;
+    const url = `${this.apiUrl}?q=${searchTerm}`;
 
-    this.http.get(url).subscribe((response: any) => {
-      // Extract the necessary information from the API response and format it into the Book interface
-      this.searchResults = response.docs.map((book: any) => ({
-        title: book.title,
-        author_name: book.author_name || ['Unknown Author'],
-        first_publish_year: book.first_publish_year || 0,
-      }));
-    });
+    this.http.get<SearchResponse>(url).subscribe(
+      (response: SearchResponse) => {
+        // Extract the necessary information from the API response and format it into the Book interface
+        this.searchResults = response.docs.map((book: any) => ({
+          title: book.title,
+          author_name: book.author_name || ['Unknown Author'],
+          first_publish_year: book.first_publish_year || 0,
+        }));
+      },
+      (error: any) => {
+        console.error('Error fetching search results:', error);
+        // Handle the error appropriately, e.g., display an error message
+      }
+    );
   }
 }
